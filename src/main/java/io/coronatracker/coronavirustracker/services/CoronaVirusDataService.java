fa9eadea099ec304681e0e21ae4ac96553fb7b2b
saahilchawande <chawandesaahil600@gmail.com>
2020-07-10 01:36:09 -0400

    Added Schedular

--- src/main/java/io/coronatracker/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/coronatracker/coronavirustracker/services/CoronaVirusDataService.java
@@ -11,6 +11,7 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +20,7 @@ public class CoronaVirusDataService {
 	private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 	
 	@PostConstruct
+	@Scheduled(cron = "* * * * * *")	// Scheduled to run every second
 	public void fetchVirusData() throws IOException, InterruptedException	{
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request =  HttpRequest.newBuilder()
